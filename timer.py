5f85b4c60060369ee4e523c1048f030c9bffe2fe
Carlos da Costa <git@cdacosta.com>
2021-04-08 13:00:11 +0100

    WIP

--- timer.py
+++ timer.py
@@ -6,36 +6,51 @@ import adafruit_dht
 import energie
 import utils
 
-temperature = -999
-humidity = -999
+
+def record_climate(dht, conn, now):
+  temperature = -999
+  humidity = -999
+  for i in range(0, 30):
+    try:
+      temperature = dht.temperature
+      humidity = dht.humidity
+      if humidity is not None and temperature is not None:
+        break
+    except RuntimeError as error:
+      # Errors happen fairly often, DHT's are hard to read, just keep going
+      print(error.args[0])
+      time.sleep(2.0)
+    except Exception as error:
+      dht.exit()
+      raise error
+
+  conn.execute('INSERT INTO climate (measured_on, temperature, humidity) VALUES(?, ?, ?)', (now.isoformat(), temperature, humidity))
+  conn.commit()
+  return temperature
+
+
 dht = adafruit_dht.DHT22(board.D3)
+recording_seconds = 0
 
-for i in range(0, 30):
-  try:
-    temperature = dht.temperature
-    humidity = dht.humidity
-    if humidity is not None and temperature is not None:
-      break
-  except RuntimeError as error:
-    # Errors happen fairly often, DHT's are hard to read, just keep going
-    print(error.args[0])
-    time.sleep(2.0)
-  except Exception as error:
-    dht.exit()
-    raise error
+while True:
+  now = datetime.datetime.utcnow()
 
-now = datetime.datetime.utcnow()
+  if recording_seconds >= 59:
+    conn = utils.get_conn()
+    temperature = record_climate(dht, conn, now)
 
-conn = utils.get_conn()
-conn.execute('INSERT INTO climate (measured_on, temperature, humidity) VALUES(?, ?, ?)', (now.isoformat(), temperature, humidity))
-conn.commit()
+    if temperature > 22:
+      print('Warm, so heater off!', temperature)
+      energie.message('0110', conn)
 
-if temperature > 22:
-  print('Warm, so heater off!', temperature)
-  energie.message('0110', conn)
+    if now.hour >= 22 and now.minute < 1:
+      print('Late, so lights off?')
+      energie.message('0111', conn)
 
-if now.hour >= 22 and now.minute < 1:
-  print('Late, so lights off?')
-  energie.message('0111', conn)
+    conn.close()
+    recording_seconds = 0
+  recording_seconds = recording_seconds + 1
 
-conn.close()
+  
+   
+  time.sleep(1) # Loop every second
--- utils.py
+++ utils.py
@@ -10,6 +10,7 @@ def get_conn():
     if not initialised:
         conn.execute('CREATE TABLE climate (id INTEGER PRIMARY KEY AUTOINCREMENT, measured_on DATETIME, temperature FLOAT, humidity FLOAT)')
         conn.execute('CREATE TABLE energie (id INTEGER PRIMARY KEY AUTOINCREMENT, messaged_on DATETIME, address TEXT)')
+        conn.execute('CREATE TABLE log (id INTEGER PRIMARY KEY AUTOINCREMENT, created_on DATETIME, message TEXT);')
         conn.commit()
 
     return sqlite3.connect(dbpath)
